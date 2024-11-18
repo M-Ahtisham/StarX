a5fcedaccba01c362006f38291a6bf84d63fa3d1
Muhammad Ahtisham Bhatti <mb04502@stud.th-deg.de>
2024-11-18 21:22:26 +0100

    Added dataset

---
+++ main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+
+
+# Text Element
+st.title("Cars Dataset 🏎️")
+st.header("This is a header") # It is smaller than Title
+st.subheader("This is a subheader") # It is smaller than Header
+st.text("Here is some text that i can write")
+st.write("* Hey, this is markdown ")
+st.divider()
+sliding_val = st.slider("This is a slider",0,100,(50))
+
+df = pd.read_csv("data/Quikr_car.csv",)
+st.dataframe(df)
+st.write("Drawing a graph of the original data sat without any changes")
+st.line_chart(df, x = "Kms_driven", y ="Price")
+
+
+# Dataset downloaded from
+# https://calmcode.io/datasets/bigmac
+
+cars = pd.read_csv("/Users/ahtisham/Desktop/Projects/Assistance Systems/QuantumCurve/data/Quikr_car.csv")
+
