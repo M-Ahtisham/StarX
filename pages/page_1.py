cd269ae79253286f60aa43f27bf6c771d3197951
Love - <love.-@stud.th-deg.de>
2024-11-21 23:35:42 +0100

    line chart

--- pages/page_1.py
+++ pages/page_1.py
@@ -16,13 +16,17 @@ df = pd.read_csv('data/Quikr_car.csv')
 st.set_page_config(layout="wide")
 
 
-col1,col2,col3=st.columns([0.1,0.8,0.1])
-# with col1:
-#     box_date=str(datetime.datetime.now().strftime("%d %B %Y"))
-#     st.write(f"Current date and time: \n{box_date}")
+col1,col2=st.columns([0.5,0.5])
 
-with col2:
+  
+with col1:
     fig = px.bar(df, x="Name", y= "Company", labels = {"Company": "Company{🛞}"},
                 title= "Company's in India", hover_data=["Company"],
                 template="gridon", height =500 )
     st.plotly_chart(fig,use_container_width=True)
+
+with col2:
+      fig1 = px.line(df, x="Kms_driven", y= "Year", labels = {"Year": "Km driven per year"},
+                title= "Yearly Driven data", hover_data=["Kms_driven"],
+                template="gridon", height =500 )
+      st.plotly_chart(fig1,use_container_width=True)
