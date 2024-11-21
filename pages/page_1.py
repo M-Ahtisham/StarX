101806aae3698d0214a0b841a39ef092b55f868d
Love - <love.-@stud.th-deg.de>
2024-11-22 00:14:39 +0100

    Page 1 created added 3 charts of data

--- pages/page_1.py
+++ pages/page_1.py
@@ -30,3 +30,41 @@ with col2:
                 title= "Yearly Driven data", hover_data=["Kms_driven"],
                 template="gridon", height =500 )
       st.plotly_chart(fig1,use_container_width=True)
+
+
+st.divider()
+
+# Grouping and aggregating data
+result1 = (
+    df.groupby(by="Location")[["Fuel_type", "Name"]]
+    .count()  # Use count or other appropriate aggregation to get DataFrame output
+    .reset_index()
+)
+
+# Plotly Figure
+fig3 = go.Figure()
+fig3.add_trace(go.Bar(x=result1["Location"], y=result1["Fuel_type"], name="Fuel_type"))
+fig3.add_trace(
+    go.Scatter(
+        x=result1["Location"],
+        y=result1["Name"],
+        name="Name",
+        mode="lines",
+        yaxis="y2",
+    )
+)
+
+fig3.update_layout(
+    title="DATA BASED ON LOCATION",
+    xaxis=dict(title="Location"),
+    yaxis=dict(title="Fuel_type", showgrid=False),
+    yaxis2=dict(title="Name", overlaying="y", side="right"),
+    template="gridon",
+    legend=dict(x=1, y=1),
+)
+
+# Display Plotly chart
+_, col3 = st.columns([0.1, 1])
+with col3:
+    st.plotly_chart(fig3, use_container_width=True)
+  
